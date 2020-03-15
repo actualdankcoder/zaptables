820e80bfc1c3ed0dc03d6ecd3ca481a755803234
DankCoder <60839576+actualdankcoder@users.noreply.github.com>
2020-03-15 12:17:26 +0530

    Update zaptables.py

--- zaptables.py
+++ zaptables.py
@@ -1,7 +1,7 @@
 from PIL import Image, ImageDraw, ImageFont
 import random
 class NewObject:
-    def __init__(self, name="OBJECT"+str(random.randint(10000,99999)),text="OBJECT",cover=ObjectTypes.Rectangle(), layer=1, bgcolor=(255,255,255),textcolor=(0,0,0),font=('cg.ttf', 20)):
+    def __init__(self, name="OBJECT"+str(random.randint(10000,99999)),text="OBJECT",cover='rectangle', layer=1, bgcolor=(255,255,255),textcolor=(0,0,0),font=('cg.ttf', 20)):
         self.layer=layer
         self.name=name
         self.text=text
@@ -9,13 +9,6 @@ class NewObject:
         self.textcolor=textcolor
         self.font=ImageFont.truetype(font[0],font[1])
         self.cover=cover
-class ObjectTypes:
-    class Rectangle:
-        pass
-    class RoundedRectangle:
-        pass
-    class Sphere:
-        pass
 
 class Chart:
     def __init__(self, width, height,background=(255,255,255)):
@@ -24,6 +17,19 @@ class Chart:
         self.canvas=Image.new('RGB',(self.width, self.height), background)
         self.draw=ImageDraw.Draw(self.canvas)
         self.tree=({},{})
+    def __length_by_chars(self, obj):
+        w=0
+        for c in self.tree[1][obj].text:
+            w+=self.tree[1][obj].font.getsize(c)[0]
+        return w
+    def __recognize_and_draw_shape(self, obj, iterator, objects):
+        if self.tree[1][obj].cover=='rectangle':
+            #self.draw.rectangle(xy=(int(iterator/(objects+1)*self.width)-self.tree[1][obj].font.getsize(self.tree[1][obj].text)-10, int(self.height/2)-4,int(iterator/(objects+1)*self.width)+self.tree[1][obj].font.getsize(self.tree[1][obj].text)[0]+10,int(self.height/2)+self.tree[1][obj].font.getsize(self.tree[1][obj].text)[1]+4),outline="black")
+            x1=int((iterator/(objects+1))*self.width)-int(self.width/(self.width/40))
+            x2=int((iterator/(objects+1))*self.width)+self.tree[1][obj].font.getsize(self.tree[1][obj].text)[0]
+            y1=int(self.height/2)-self.height/(self.height/10)
+            y2=int(self.height/2)+self.tree[1][obj].font.getsize(self.tree[1][obj].text)[1]+self.height/(self.height/10)
+            self.draw.rectangle(xy=(x1,y1,x2,y2),outline='black')
     def add_object(self, obj):
         if isinstance(obj, NewObject):
             self.tree[0][obj.name]={}
@@ -36,7 +42,9 @@ class Chart:
         for obj in self.tree[0]:
             iterator+=1
             self.draw.text((int(int(float(iterator/(objects+1))*self.width)-int(self.width/((1/20)*self.width))), int((1/2)*self.height)), self.tree[1][obj].text, self.tree[1][obj].textcolor, font=self.tree[1][obj].font)
+            self.__recognize_and_draw_shape(obj, iterator, objects)
         return self.canvas
+
 class Errors:
     class InvalidObject(Exception):
         """Raised when the object passed is not of the correct type"""
